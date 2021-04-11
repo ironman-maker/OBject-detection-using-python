4394ba436cf02dfcf6fb8fce73e51497e32f66f8
Anmol Hashmi <59932310+ironman-maker@users.noreply.github.com>
2021-04-11 14:12:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import cv2
 
 thres = 0.5 # Threshold to detect object
 nms_threshold = 0.2 #(0.1 to 1) 1 means no suppress , 0.1 means high suppress 
-cap = cv2.VideoCapture('Road_traffic_video2.mp4')
+cap = cv2.VideoCapture('demo.mp4')
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,280) #width 
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,120) #height 
 cap.set(cv2.CAP_PROP_BRIGHTNESS,150) #brightness 
 
 classNames = []
-with open('model.names','r') as f:
+with open('objects.names','r') as f:
     classNames = f.read().splitlines()
 print(classNames)
 
